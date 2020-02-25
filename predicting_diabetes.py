631454458d1465dc1586b05a924638c539f0a172
Pablo ( Paulo )  Lopez <pau_lo_lop_ez@protonmail.com>
2020-02-25 14:21:40 -0800

    created a model for predicting diabetes

--- predicting_diabetes.py
+++ predicting_diabetes.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import math
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
@@ -26,7 +27,6 @@ class main():
 
     # checking for NaN values
     # print(df.isnull().sum())
-    #                            
     # split into train/test
     X = df.iloc[:, 0:8]
     y = df.iloc[:, 8]
@@ -35,18 +35,30 @@ class main():
     # feature scaling 
     # rule of thumb:  any algorithm that computes distance or assumes normality
     # scale features.. exclude tree classifiers.
-    scaling_X = StandardScaler() # set data from -1 to 1
+    # set data from -1 to 1
+    scaling_X = StandardScaler()
     # fitting only the train set
     X_train = scaling_X.fit_transform(X_train)
     # yet, make sure the x going in it's transform
     X_test = scaling_X.transform(X_test)
 
-    # load the classifer
-    clf = KNeighborsClassifier()
+    # load the classifier
+    # N_neighbors = K
+    # print(math.sqrt(len(y_test))) # this gives ~ 12.4 we'll use 11
+    # p == power parameter to define the metric 2 classes
+    clf = KNeighborsClassifier(n_neighbors=11, p=2, metric='euclidean')
 
-    # fit and predict
-    # clf.fit(X_train, y_train)
-    # predictions = clf.predict(X_test)
-    # score = accuracy_score(y_test, predictions)
-    # print('Accuracy: {0:f}'.format(score))
-
+    # fit and predict the train data in the model
+    clf.fit(X_train, y_train)
+    # predictions
+    y_pred = clf.predict(X_test)
+    # print(y_pred)
+    score = accuracy_score(y_test, y_pred)
+    print('Accuracy score: {0:f}'.format(score))
+    # Evaluate model
+    cm = confusion_matrix(y_test, y_pred)
+    print("Confusion Matrix: \n", cm)
+    # f1 score measures the test's accuracy considering both 
+    # precision and recall
+    f_score = f1_score(y_test, y_pred)
+    print("f1 score: {0:f}".format(f_score))
