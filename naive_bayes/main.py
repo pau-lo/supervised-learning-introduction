c9b44de3c699dec8708ab88655664187b9e54e6d
Pablo ( Paulo )  Lopez <pau_lo_lop_ez@protonmail.com>
2020-02-25 20:01:18 -0800

    added a naive_bayes classifier for news categories

--- naive_bayes/main.py
+++ naive_bayes/main.py
@@ -1,3 +1,11 @@
+# Use case text-classification
+# performing text classification on news headlines and classify
+# news into different topics for news website
+
+from sklearn.metrics import confusion_matrix
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.pipeline import make_pipeline
 from sklearn.datasets import fetch_20newsgroups
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,6 +19,7 @@ class main():
     data = df.target_names
     print(data)
 
+    # defining all the categories
     categories = ['alt.atheism', 'comp.graphics', 'comp.os.ms-windows.misc',
                   'comp.sys.ibm.pc.hardware', 'comp.sys.mac.hardware', 'comp.windows.x',
                   'misc.forsale', 'rec.autos', 'rec.motorcycles', 'rec.sport.baseball',
@@ -18,50 +27,35 @@ class main():
                   'soc.religion.christian', 'talk.politics.guns', 'talk.politics.mideast',
                   'talk.politics.misc', 'talk.religion.misc']
 
-    # # prepraring data
-    # # replacing zeroes in datasets with Nan and the filled them with the mean
-    # zero_not_accepted = ['Glucose', 'BloodPressure', 'SkinThickness', 'BMI', 'Insulin']
+    # training data on these categories
+    train = fetch_20newsgroups(subset='train', categories=categories)
 
-    # for column in zero_not_accepted:
-    #     df[column] = df[column].replace(0, np.NaN)
-    #     mean = int(df[column].mean(skipna=True))
-    #     df[column] = df[column].replace(np.NaN, mean)
-    # # print(df.head(10))
+    # Testing the data for these categories
+    test = fetch_20newsgroups(subset='test', categories=categories)
 
-    # # checking for NaN values
-    # # print(df.isnull().sum())
-    # # split into train/test
-    # X = df.iloc[:, 0:8]
-    # y = df.iloc[:, 8]
-    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = .2, random_state=0)
+    # printing the trainning data
+    # print(train.data[5])
 
-    # # feature scaling
-    # # rule of thumb:  any algorithm that computes distance or assumes normality
-    # # scale features.. exclude tree classifiers.
-    # # set data from -1 to 1
-    # scaling_X = StandardScaler()
-    # # fitting only the train set
-    # X_train = scaling_X.fit_transform(X_train)
-    # # yet, make sure the x going in it's transform
-    # X_test = scaling_X.transform(X_test)
+    # creating model
+    model = make_pipeline(TfidfVectorizer(), MultinomialNB())
 
-    # # load the classifier
-    # # N_neighbors = K
-    # # print(math.sqrt(len(y_test))) # this gives ~ 12.4 we'll use 11
-    # # p == power parameter to define the metric 2 classes
-    # clf = KNeighborsClassifier(n_neighbors=11, p=2, metric='euclidean')
+    # Trainning the model with the train data
+    model.fit(train.data, train.target)
 
-    # # fit and predict the train data in the model
-    # clf.fit(X_train, y_train)
-    # # predictions
-    # y_pred = clf.predict(X_test)
-    # # print(y_pred)
-    # score = accuracy_score(y_test, y_pred)
-    # print('Accuracy score: {0:f}'.format(score))
-    # # Evaluate model
-    # cm = confusion_matrix(y_test, y_pred)
-    # print("Confusion Matrix: \n", cm)
-    # # f1 score measures the test's accuracy considering both
-    # # precision and recall
-    # f_score = f1_score(y_test, y_pred)
-    # print("f1 score: {0:f}".format(f_score))
+    # creating labels for the test data
+    labels = model.predict(test.data)
+
+    # creating confusion matrix and heat map
+    cm = confusion_matrix(test.target, labels)
+    sns.heatmap(cm.T, square=True, annot=True, fmt='d', cbar=False,
+                xticklabels=train.target_names, yticklabels=train.target_names)
+
+    # plotting heatmap of confusion matrix
+    plt.xlabel('True label')
+    plt.ylabel('Predicted label')
+
+    # predicting category on new data on trained model
+    def predict_category(s, train=train, model=model):
+        pred = model.predict(s)
+        return train.target_names[pred[0]]
+
